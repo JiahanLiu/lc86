700a695c0058d111ea26d255db8b507d840d0066
stevenflo <stevenflolid@hotmail.com>
2018-03-25 15:03:35 -0500

    Added the barrel shifter
    Barrel shifter logic is in place, overall delay should hopefully not be horrible

--- pipeline/fetch.v
+++ pipeline/fetch.v
@@ -20,3 +20,43 @@ module fetch (
 );
 
 endmodule
+
+module shift128$(input [127:0] Din_low,
+		input [127:0] Din_high,
+		input dir,
+		input [3:0] amnt,
+		output [127:0] Dout);
+	wire [3:0] array [127:0];
+	wire [1:0] mux_array [127:0];
+	genvar i;
+for(i=0;i<8;i=i+1)
+  begin : generate_loop
+  //Allowed since i is constant when the loop is unrolled
+  assign array[i] = {Din_high[127-i*8:0], Din_low[127:127-i*8]};
+  end
+
+//muxes to select shifted value, first round of muxes
+mux4_128$(mux_array[0],array[0],array[1],array[2],array[3],amnt[0],amnt[1]);
+mux4_128$(mux_array[1],array[4],array[5],array[6],array[7],amnt[0],amnt[1]);
+mux4_128$(mux_array[2],array[8],array[9],array[10],array[11],amnt[0],amnt[1]);
+mux4_128$(mux_array[3],array[12],array[13],array[14],array[15],amnt[0],amnt[1]);
+
+//last round of muxes
+mux4_16$(Dout,mux_array[0],mux_array[0],mux_array[0],mux_array[0],amnt[2],amnt[3]);
+	
+
+endmodule
+
+module mux4_128$(output [127:0] Y,
+		input [127:0] IN0,input [127:0] IN1, input [127:0] IN2,input [127:0] IN3,
+		input S0, input S1);
+genvar i;
+for(i=0;i<8;i=i+1)
+  begin : generate_loop
+	mux4_16$(Y[i*8 +7:i*8], IN0[i*8 +7:i*8], IN1[i*8 +7:i*8], IN2[i*8 +7:i*8], IN3[i*8 +7:i*8],
+			S0, S1);
+  end
+
+endmodule
+
+
