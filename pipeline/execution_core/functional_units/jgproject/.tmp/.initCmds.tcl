a09b229d663810940d0df74197a862f0f1e0095e
Steven Gregory Flolid <sflolid@bowser.ece.utexas.edu>
2018-04-16 19:10:20 -0500

    Removing old directory locations

--- pipeline/execution_core/functional_units/jgproject/.tmp/.initCmds.tcl
+++
@@ -1,2 +0,0 @@
-@puts "INFO: successfully checked out licenses \"jasper_interactive\" and \"jasper_fpv\"."
-@puts "INFO: reading configuration file \"/home/ecelrc/students/anarkhede/.config/jasper/jaspergold.conf\"."
--- pipeline/fetch_decode.v
+++
@@ -1,2 +0,0 @@
-// fetch_buffer (30 bytes)
-// buffer (15 bytes) | 0-3 prefix |  
