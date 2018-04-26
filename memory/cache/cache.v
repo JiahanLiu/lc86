e9ec0afdafc98c034ec955f01cc290c7b783bb52
Steven Gregory Flolid <sflolid@bowser.ece.utexas.edu>
2018-04-26 16:53:22 -0500

    Just switching from and gates to muxes

--- memory/cache/cache.v
+++ memory/cache/cache.v
@@ -78,13 +78,16 @@ module cache( //interface with the processor
 
    //CHECKING FOR A HIT
    wire 	     MATCH;
+   wire 	     INVALID = 1'b0;
    equalitycheck equalitycheck_u(MATCH, tagstore_tag, address[15:9]);
-   and2$ and_u(HIT, MATCH, tagstore_V);
-
+   mux2$ mux_hit(HIT, INVALID, MATCH, tagstore_V);
+   
    //CHECKING FOR AN EVICT
    wire 	     MATCH_INV;
    inv1$ MATCH_INVER(MATCH_INV, MATCH);
-   and2$ and_evict(evict, MATCH_INV, tagstore_V);
+   mux2$ mux_evict(evict, INVALID, MATCH_INV, tagstore_V);
+   
+   
    
 
 
