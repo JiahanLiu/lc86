8910970b9a3780711a2b24a97eaec59c8d1abb8b
Nelson Wu <ntw248@utexas.edu>
2018-05-05 18:52:07 -0500

    new writeback connections

--- pipeline/pipeline_caches.v
+++ pipeline/pipeline_caches.v
@@ -1490,6 +1490,12 @@ module PIPELINE(CLK, CLR, PRE, IR);
     reg32e$ u_EX_de_ld_gpr1_wb_latch (CLK, {31'b0, EX_d2_ld_gpr1_ex_next}, EX_d2_ld_gpr1_ex_out, ,CLR,PRE,LD_EX);
     assign EX_d2_ld_gpr1_ex = EX_d2_ld_gpr1_ex_out[0]; 
 
+    wire EX_d2_ld_mm_ex_next = ME2_D2_LD_MM_WB_OUT;
+    wire [31:0] EX_d2_ld_mm_ex_out;
+    wire EX_d2_ld_mm_ex;
+    reg32e$ u_EX_d2_ld_mm_wb_latch (CLK, {31'b0, EX_d2_ld_mm_ex_next}, EX_d2_ld_mm_ex_out, ,CLR,PRE,LD_EX);
+    assign EX_d2_ld_mm_ex = EX_d2_ld_mm_ex_out[0];
+   
     wire EX_d2_dcache_write_ex_next = ME2_D2_MEM_WR_WB_OUT;
     wire [31:0] EX_d2_dcache_write_ex_out;
     wire EX_d2_dcache_write_ex; 
@@ -1545,7 +1551,8 @@ module PIPELINE(CLK, CLR, PRE, IR);
 
     wire [1:0] WB_de_datasize_all_next;
     wire WB_ex_ld_gpr1_wb_next;
-    wire WB_ex_ld_gpr2_wb_next; 
+    wire WB_ex_ld_gpr2_wb_next;
+    wire WB_d2_ld_mm_wb_next; 
     wire WB_ex_dcache_write_wb_next; 
     wire WB_de_repne_wb_next; 
 
@@ -1571,6 +1578,7 @@ module PIPELINE(CLK, CLR, PRE, IR);
         EX_d2_datasize_all,
         EX_d2_aluk_ex, 
         EX_d2_ld_gpr1_ex,
+	EX_d2_ld_mm_ex,
         EX_d2_dcache_write_ex, 
         EX_d2_repne_wb, 
 
@@ -1598,7 +1606,8 @@ module PIPELINE(CLK, CLR, PRE, IR);
 
         WB_de_datasize_all_next,
         WB_ex_ld_gpr1_wb_next,
-        WB_ex_ld_gpr2_wb_next, 
+        WB_ex_ld_gpr2_wb_next,
+	WB_d2_ld_mm_wb_next,
         WB_ex_dcache_write_wb_next, 
         WB_de_repne_wb_next, 
 
@@ -1715,6 +1724,11 @@ module PIPELINE(CLK, CLR, PRE, IR);
     reg32e$ u_WB_ex_ld_gpr2_wb_latch(CLK, {31'b0, WB_ex_ld_gpr2_wb_next}, WB_ex_ld_gpr2_wb_out, ,CLR,PRE,LD_WB);
     assign WB_ex_ld_gpr2_wb = WB_ex_ld_gpr2_wb_out[0]; 
 
+    wire [31:0] WB_d2_ld_mm_wb_out;
+    wire WB_d2_ld_mm_wb;
+    reg32e$ u_WB_d2_ld_mm_wb_latch(CLK, {31'b0, WB_d2_ld_mm_wb_next}, WB_d2_ld_mm_wb_out, ,CLR,PRE,LD_WB);
+    assign WB_d2_ld_mm_wb = WB_d2_ld_mm_wb_out[0];
+   
     wire [31:0] WB_ex_dcache_write_wb_out; 
     wire WB_ex_dcache_write_wb; 
     reg32e$ u_WB_ex_dcache_write_wb_latch(CLK, {31'b0, WB_ex_dcache_write_wb_next}, WB_ex_dcache_write_wb_out, ,CLR,PRE,LD_WB);
@@ -1769,6 +1783,7 @@ module PIPELINE(CLK, CLR, PRE, IR);
         WB_de_datasize_all,
         WB_ex_ld_gpr1_wb,
         WB_ex_ld_gpr2_wb,
+	WB_d2_ld_mm_wb,
         WB_ex_dcache_write_wb,
         WB_de_repne_wb, 
 
