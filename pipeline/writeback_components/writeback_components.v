686faf0e3a79e303c24ce5c2b2e1fbbcc31f2e07
Unknown <jiahan.jim.liu@gmail.com>
2018-05-06 16:41:27 -0500

    Fixed EIP = x

--- pipeline/writeback_components/writeback_components.v
+++ pipeline/writeback_components/writeback_components.v
@@ -144,7 +144,7 @@ module validate_signals_wb(
 	wire regular_ld_eip, v_repne_second_uop_cmps;
 	and3$ u_v_ssecond_uop_of_repne(v_repne_second_uop_cmps, CS_IS_CMPS_SECOND_UOP_ALL, WB_d2_repne_wb, WB_V);
 	and2$ u_and_regular_eip(regular_ld_eip, WB_V, cs_ld_eip);
-	mux2$ u_and_final_eip(v_cs_ld_eip, regular_ld_eip, wb_repne_terminate_all, repne_second_uop_cmps);
+	mux2$ u_and_final_eip(v_cs_ld_eip, regular_ld_eip, wb_repne_terminate_all, v_repne_second_uop_cmps);
 	and2$ u_and_cs(v_cs_ld_cs, WB_V, CS_LD_CS_WB);
 
 endmodule // validate_signals_wb
