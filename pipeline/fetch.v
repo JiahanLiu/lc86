1491802af85090b42b8fb64322b3b9cfb754ff45
Nelson Wu <ntw248@utexas.edu>
2018-05-09 14:18:02 -0500

    more fetch fix

--- pipeline/fetch.v
+++ pipeline/fetch.v
@@ -409,9 +409,11 @@ next_read_ptr = if (dep_stall) ? Read_ptr : (read_ptr + length)
 
    wire and_empty_flush_out;
    and3$ and_empty_flush (and_empty_flush_out, flush_state, flush_bar, ifu_icache_rd_stall_out_bar);
-   or3$
-      or_empty2 (or_empty2_out, or_empty0_out, or_empty1_out, and_empty_flush_out);
-   assign Dempty_state = or_empty2_out;
+   or2$
+      or_empty2 (or_empty2_out, or_empty0_out, or_empty1_out);
+   and2$ and_empty_fix (and_empty_fix_out, or_empty2_out, and_empty_flush_out);
+   //assign Dempty_state = or_empty2_out;
+   assign Dempty_state = and_empty_fix_out;
 
    wire and_full0_out, and_full1_out, and_full2_out, and_full3_out,
         and_full4_out, and_full5_out, and_full6_out, or_full0_out;
